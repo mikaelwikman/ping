0d72edd7d7ee902b845ff7d55cf1e9bfbac7f6d0
Mikael Wikman <mikael@swedcontent.com>
2014-10-28 12:25:37 +0100

    Newlines are nice

--- lib/pingdom.rb
+++ lib/pingdom.rb
@@ -9,5 +9,5 @@ raw_data.shift
 aggregated_data = p.process(raw_data)
 
 aggregated_data.each do |checkid, start_time, end_time, status|
-  printf "%-3s %-10s %-10s %17s", checkid, start_time, end_time, status
+  printf "%-3s %-10s %-10s %17s\n", checkid, start_time, end_time, status
 end
