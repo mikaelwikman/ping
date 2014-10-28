3193d7560288b7e583f69dbd1f0cdf2cea7b187e
Mikael Wikman <mikael@swedcontent.com>
2014-10-28 12:23:53 +0100

    Added file for execution

---
+++ lib/pingdom.rb
@@ -0,0 +1,13 @@
+require 'pingdom/processor'
+
+sort = eval($1.to_s)
+p = Processor.new(sort)
+
+raw_data = STDIN.readlines.map{|r| r.split(',').map{|e| e.strip}}
+raw_data.shift
+
+aggregated_data = p.process(raw_data)
+
+aggregated_data.each do |checkid, start_time, end_time, status|
+  printf "%-3s %-10s %-10s %17s", checkid, start_time, end_time, status
+end
